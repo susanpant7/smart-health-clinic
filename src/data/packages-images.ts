2232739af3e64affcef03ab79748afb633b56cfa
susanpant7 <susan.pant7@gmail.com>
2026-07-01 14:26:34 -0400

    packages images added

---
+++ src/data/packages-images.ts
@@ -0,0 +1,16 @@
+export const packageImages =
+[
+    '/images/packages/package1.jpg',
+    '/images/packages/package2.jpg',
+    '/images/packages/package3.jpg',
+    '/images/packages/package4.jpg',
+    '/images/packages/package5.jpg',
+    '/images/packages/package6.jpg',
+    '/images/packages/package7.jpg',
+    '/images/packages/package8.jpg',
+    '/images/packages/package9.jpg',
+    '/images/packages/package10.jpg',
+    '/images/packages/package11.jpg',
+    '/images/packages/package12.jpg',
+    '/images/packages/package13.jpg', 
+]
